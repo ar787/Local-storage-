8ffb717f0e5213a2a0d5e88c994f1cdfa9f4d551
ar787 <armanhovhannisyan1002@gmail.com>
2021-12-06 22:56:11 +0400

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
-import {
-  Switch,
-  useLocation,
-  // Link
-} from "react-router-dom";
+import { Switch, useLocation } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import SignUp from './components/views/sign-up/signUp';
 import SignIn from './components/views/sign-in/siginIn';
@@ -14,21 +9,21 @@ import { auth } from './firebase-config/firebase-config'
 import { PublicRoute, PrivateRoute } from './routes/routesType';
 import LoadingPage from './components/elements/loading-page/loadingPage';
 
+import './App.css';
+
 function App() {
   const [isAuthenticated, setAuthenticated] = useState(false)
   const [loading, setLoading] = useState(true)
   const location = useLocation()
-  // let isAuthenticated = null
+
   useEffect(() => {
     onAuthStateChanged(auth, user => {
       if (user) {
         setAuthenticated(true)
         setLoading(false)
-        // isAuthenticated = user.getIdToken()
       } else {
         setAuthenticated(false)
         setLoading(false)
-        // isAuthenticated = null
       }
     })
   }, [])
@@ -36,7 +31,6 @@ function App() {
 
 
     loading ? <LoadingPage /> : (
-      // <Router>
       <TransitionGroup>
         <CSSTransition
           key={location.pathname}
@@ -50,7 +44,6 @@ function App() {
           </Switch>
         </CSSTransition>
       </TransitionGroup>
-      // </Router >
     )
   );
 }
